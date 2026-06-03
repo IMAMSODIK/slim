1573d4be6f948f4aef3b41d92428513501cb4a13
IMAM SODIK <imamsodik00@gmail.com>
2026-06-04 05:20:41 +0700

    asdf

--- sysconfig.inc.php
+++ sysconfig.inc.php
@@ -32,7 +32,8 @@ if (!defined('INDEX_AUTH')) {
  *
  * In production mode, the system error message will be disabled
  */
-define('ENVIRONMENT', 'development'); // value can be 'development' or 'production'
+define('ENVIRONMENT', 'production');
+// define('ENVIRONMENT', 'development'); // value can be 'development' or 'production'
 
 switch (ENVIRONMENT) {
   case 'development':
