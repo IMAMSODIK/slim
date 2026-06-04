b7f0a9c1b7583d18d4f5a999480dbe8b84337d82
IMAM SODIK <imamsodik00@gmail.com>
2026-06-04 16:43:34 +0700

    asfd

--- api/v1/routes.php
+++ api/v1/routes.php
@@ -32,7 +32,7 @@ $router->map('GET', '/subject/latest', 'SubjectController@getLatest');
 $router->map('GET', '/member/top', 'MemberController@getTopMember');
 $router->map('GET', '/biblio/gmd/[*:gmd]', 'BiblioController@getByGmd');
 $router->map('GET', '/biblio/coll_type/[*:coll_type]', 'BiblioController@getByCollType');
-$router->map('GET', '/biblio', 'BiblioController@getAll');
+$router->map('GET', '/mobile/biblio', 'BiblioController@getAll');
 
 /*----------  Admin  ----------*/
 $router->map('GET', '/biblio/total/all', 'BiblioController@getTotalAll');
