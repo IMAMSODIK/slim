90094dcd16a818678abf162f694ed536807b1f4b
IMAM SODIK <imamsodik00@gmail.com>
2026-06-06 16:05:52 +0700

    adsf

--- admin/modules/informasi/index.php
+++ admin/modules/informasi/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Copyright (C) 2007,2008,2009,2010  Arie Nugraha (dicarve@yahoo.com)
  * Modified for News/Information Module
@@ -34,6 +35,9 @@ define('DB_ACCESS', 'fa');
 if (!defined('SB')) {
     // main system configuration
     require '../../../sysconfig.inc.php';
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
     // start the session
     require SB . 'admin/default/session.inc.php';
 }
@@ -99,13 +103,13 @@ if (isset($_POST['removeImage']) && isset($_POST['nimg']) && isset($_POST['img']
 }
 
 /* RECORD OPERATION */
-if (isset($_POST['saveData']) AND $can_read AND $can_write) {
+if (isset($_POST['saveData']) and $can_read and $can_write) {
     if (!simbio_form_maker::isTokenValid()) {
         utility::jsToastr('Informasi', __('Invalid form submission token!'), 'error');
         utility::writeLogs($dbs, 'staff', $_SESSION['uid'], 'system', 'Invalid form submission token, might be a CSRF attack from ' . $_SERVER['REMOTE_ADDR']);
         exit();
     }
-    
+
     $judul = trim(strip_tags($_POST['judul']));
     // check form validity
     if (empty($judul)) {
@@ -119,7 +123,7 @@ if (isset($_POST['saveData']) AND $can_read AND $can_write) {
         $data['updated_at'] = date('Y-m-d H:i:s');
 
         // image uploading
-        if (!empty($_FILES['gambar']) AND $_FILES['gambar']['size']) {
+        if (!empty($_FILES['gambar']) and $_FILES['gambar']['size']) {
             // create upload object
             $image_upload = new simbio_file_upload();
             $image_upload->setAllowableFormat($sysconf['allowed_images']);
@@ -147,7 +151,7 @@ if (isset($_POST['saveData']) AND $can_read AND $can_write) {
                 . preg_replace("/[^a-zA-Z0-9]+/", "_", $data['judul'])
                 . '.' . $filedom);
 
-            if ($valid AND file_put_contents(IMGBS . 'docs/' . $new_filename, $filedata)) {
+            if ($valid and file_put_contents(IMGBS . 'docs/' . $new_filename, $filedata)) {
                 $data['gambar'] = $dbs->escape_string($new_filename);
                 if (!defined('UPLOAD_SUCCESS')) define('UPLOAD_SUCCESS', 1);
                 $upload_status = UPLOAD_SUCCESS;
@@ -156,13 +160,13 @@ if (isset($_POST['saveData']) AND $can_read AND $can_write) {
 
         // create sql op object
         $sql_op = new simbio_dbop($dbs);
-        
+
         if (isset($_POST['updateRecordID'])) {
             /* UPDATE RECORD MODE */
             // remove created_at
             unset($data['created_at']);
             // filter update record ID
-            $updateRecordID = (integer)$_POST['updateRecordID'];
+            $updateRecordID = (int)$_POST['updateRecordID'];
             \SLiMS\Plugins::getInstance()->execute('informasi_before_update', ['data' => array_merge($data, ['id' => $updateRecordID])]);
             // update data
             $update = $sql_op->update('informasi', $data, 'id=' . $updateRecordID);
@@ -212,8 +216,8 @@ if (isset($_POST['saveData']) AND $can_read AND $can_write) {
         exit();
     }
     exit();
-} else if (isset($_POST['itemID']) AND !empty($_POST['itemID']) AND isset($_POST['itemAction'])) {
-    if (!($can_read AND $can_write)) {
+} else if (isset($_POST['itemID']) and !empty($_POST['itemID']) and isset($_POST['itemAction'])) {
+    if (!($can_read and $can_write)) {
         die();
     }
     if (!simbio_form_maker::isTokenValid()) {
@@ -225,21 +229,21 @@ if (isset($_POST['saveData']) AND $can_read AND $can_write) {
     // create sql op object
     $sql_op = new simbio_dbop($dbs);
     $error_num = 0;
-    
+
     if (!is_array($_POST['itemID'])) {
         // make an array
-        $_POST['itemID'] = array((integer)$_POST['itemID']);
+        $_POST['itemID'] = array((int)$_POST['itemID']);
     }
-    
+
     // loop array
     foreach ($_POST['itemID'] as $itemID) {
-        $itemID = (integer)$itemID;
-        
+        $itemID = (int)$itemID;
+
         // get news title for logging
         $_sql_news_q = sprintf('SELECT judul FROM informasi WHERE id=%d', $itemID);
         $news_q = $dbs->query($_sql_news_q);
         $news_d = $news_q->fetch_row();
-        
+
         if (!$sql_op->delete('informasi', "id=$itemID")) {
             $error_num++;
         } else {
@@ -249,7 +253,7 @@ if (isset($_POST['saveData']) AND $can_read AND $can_write) {
             utility::writeLogs($dbs, 'staff', $_SESSION['uid'], 'informasi', $_SESSION['realname'] . ' DELETE news data (' . $news_d[0] . ') with id (' . $itemID . ')');
         }
     }
-    
+
     // error alerting
     if ($error_num == 0) {
         utility::jsToastr('Informasi', __('All Data Successfully Deleted'), 'success');
@@ -264,7 +268,7 @@ if (isset($_POST['saveData']) AND $can_read AND $can_write) {
 
 if (!$in_pop_up) {
     /* search form */
-    ?>
+?>
     <div class="menuBox">
         <div class="menuBoxInner biblioIcon">
             <div class="per_title">
@@ -273,13 +277,13 @@ if (!$in_pop_up) {
             <div class="sub_section">
                 <div class="btn-group">
                     <a href="<?php echo MWB; ?>informasi/index.php"
-                       class="btn btn-default"><?php echo __('News List'); ?></a>
+                        class="btn btn-default"><?php echo __('News List'); ?></a>
                     <a href="<?php echo MWB; ?>informasi/index.php?action=detail"
-                       class="btn btn-default"><?php echo __('Add News'); ?></a>
+                        class="btn btn-default"><?php echo __('Add News'); ?></a>
                 </div>
                 <form name="search" action="<?php echo MWB; ?>informasi/index.php" id="search" method="get"
-                      class="form-inline"><?php echo __('Search'); ?>
-                    <input type="text" name="keywords" id="keywords" class="form-control col-md-3"/>
+                    class="form-inline"><?php echo __('Search'); ?>
+                    <input type="text" name="keywords" id="keywords" class="form-control col-md-3" />
                     <select name="field" class="form-control col-md-2">
                         <option value="0"><?php echo __('All Field'); ?></option>
                         <option value="judul"><?php echo __('Title'); ?></option>
@@ -291,30 +295,30 @@ if (!$in_pop_up) {
                         <option value="publish"><?php echo __('Publish'); ?></option>
                     </select>
                     <input type="submit" id="doSearch" value="<?php echo __('Search'); ?>"
-                           class="s-btn btn btn-default"/>
+                        class="s-btn btn btn-default" />
                 </form>
             </div>
         </div>
     </div>
-    <?php
+<?php
     /* search form end */
 }
 
 /* main content */
-if (isset($_POST['detail']) OR (isset($_GET['action']) AND $_GET['action'] == 'detail')) {
-    if ((isset($_GET['action'])) AND ($_GET['action'] == 'detail')) {
+if (isset($_POST['detail']) or (isset($_GET['action']) and $_GET['action'] == 'detail')) {
+    if ((isset($_GET['action'])) and ($_GET['action'] == 'detail')) {
         $log = new AlLibrarian('1153', array("username" => $_SESSION['uname'], "uid" => $_SESSION['uid'], "realname" => $_SESSION['realname']));
-    } elseif ((isset($_GET['itemID'])) AND (isset($_GET['detail'])) AND ($_GET['detail'] == true)) {
+    } elseif ((isset($_GET['itemID'])) and (isset($_GET['detail'])) and ($_GET['detail'] == true)) {
         $log = new AlLibrarian('1155', array("username" => $_SESSION['uname'], "uid" => $_SESSION['uid'], "realname" => $_SESSION['realname'], "news_id" => $_GET['itemID']));
     }
 
-    if (!($can_read AND $can_write)) {
+    if (!($can_read and $can_write)) {
         die('<div class="errorBox">' . __('You are not authorized to view this section') . '</div>');
     }
-    
+
     /* RECORD FORM */
     // try query
-    $itemID = (integer)isset($_POST['itemID']) ? $_POST['itemID'] : (isset($_GET['itemID']) ? $_GET['itemID'] : 0);
+    $itemID = (int)isset($_POST['itemID']) ? $_POST['itemID'] : (isset($_GET['itemID']) ? $_GET['itemID'] : 0);
     $_sql_rec_q = sprintf('SELECT * FROM informasi WHERE id=%d', $itemID);
     $rec_q = $dbs->query($_sql_rec_q);
     $rec_d = $rec_q->fetch_assoc();
@@ -349,20 +353,38 @@ if (isset($_POST['detail']) OR (isset($_GET['action']) AND $_GET['action'] == 'd
 
     /* Form Element(s) */
     // News title
-    $form->addTextField('text', 'judul', __('Title') . '*', $rec_d['judul'] ?? '', 'class="form-control"',
-        __('News title'));
+    $form->addTextField(
+        'text',
+        'judul',
+        __('Title') . '*',
+        $rec_d['judul'] ?? '',
+        'class="form-control"',
+        __('News title')
+    );
 
     // News content
-    $form->addTextField('textarea', 'isi', __('Content'), $rec_d['isi'] ?? '', 'class="form-control ckeditor" rows="10"',
-        __('News content/article'));
+    $form->addTextField(
+        'textarea',
+        'isi',
+        __('Content'),
+        $rec_d['isi'] ?? '',
+        'class="form-control ckeditor" rows="10"',
+        __('News content/article')
+    );
 
     // News status
     $status_options = array(
         array('draft', __('Draft')),
         array('publish', __('Publish'))
     );
-    $form->addSelectList('status', __('Status'), $status_options, $rec_d['status'] ?? 'draft', 'class="select2"',
-        __('News publication status'));
+    $form->addSelectList(
+        'status',
+        __('Status'),
+        $status_options,
+        $rec_d['status'] ?? 'draft',
+        'class="select2"',
+        __('News publication status')
+    );
 
     // News image
     $str_input = '<div class="row">';
@@ -399,7 +421,7 @@ if (isset($_POST['detail']) OR (isset($_GET['action']) AND $_GET['action'] == 'd
     $str_input .= '</div>';
     $str_input .= '<textarea id="base64picstring" name="base64picstring" style="display: none;"></textarea>';
     $str_input .= '</div></div></div>';
-    
+
     $form->addAnything(__('Image'), $str_input);
 
     // edit mode message
@@ -412,10 +434,10 @@ if (isset($_POST['detail']) OR (isset($_GET['action']) AND $_GET['action'] == 'd
     }
     // print out the form object
     echo $form->printOut();
-    ?>
+?>
     <script type="text/javascript">
-        $(document).ready(function () {
-            $('.removeImage').click(function (e) {
+        $(document).ready(function() {
+            $('.removeImage').click(function(e) {
                 if (confirm('Are you sure you want to permanently remove this image?')) {
                     return true;
                 } else {
@@ -423,11 +445,11 @@ if (isset($_POST['detail']) OR (isset($_GET['action']) AND $_GET['action'] == 'd
                 }
             });
 
-            $(document).on('change', '.custom-file-input', function () {
+            $(document).on('change', '.custom-file-input', function() {
                 var input = document.querySelector("#customFile");
                 var fReader = new FileReader();
                 fReader.readAsDataURL(input.files[0]);
-                fReader.onloadend = function (event) {
+                fReader.onloadend = function(event) {
                     var img = document.querySelector("#imageFilename img");
                     img.src = event.target.result;
                 }
@@ -435,9 +457,11 @@ if (isset($_POST['detail']) OR (isset($_GET['action']) AND $_GET['action'] == 'd
                 $(this).parent('.custom-file').find('.custom-file-label').text(fileName);
             });
 
-            $('#getImage').click(function () {
-                $.post("<?php echo MWB ?>bibliography/scrape_image.php", {imageURL: $('#getUrl').val()})
-                    .done(function (data) {
+            $('#getImage').click(function() {
+                $.post("<?php echo MWB ?>bibliography/scrape_image.php", {
+                        imageURL: $('#getUrl').val()
+                    })
+                    .done(function(data) {
                         if (data.status == 'VALID') {
                             $('#base64picstring').val(data.image);
                             $('#imageFilename img').attr('src', data.message);
@@ -465,113 +489,121 @@ if (isset($_POST['detail']) OR (isset($_GET['action']) AND $_GET['action'] == 'd
             });
         });
     </script>
-    <?php
+<?php
 } else {
     # ADV LOG SYSTEM - STIIL EXPERIMENTAL
     $log = new AlLibrarian('1151', array("username" => $_SESSION['uname'], "uid" => $_SESSION['uid'], "realname" => $_SESSION['realname']));
 
     require SIMBIO . 'simbio_UTILS/simbio_tokenizecql.inc.php';
-    
+
     // number of records to show in list
     $news_result_num = ($sysconf['biblio_result_num'] > 100) ? 100 : $sysconf['biblio_result_num'];
 
     // create datagrid
     $datagrid = new simbio_datagrid();
-    
+
     // table spec
     $table_spec = 'informasi';
     $str_criteria = '1=1';
-    
-    if ($can_read AND $can_write) {
-        $datagrid->setSQLColumn('id', 'judul AS \'' . __('Title') . '\'',
+
+    if ($can_read and $can_write) {
+        $datagrid->setSQLColumn(
+            'id',
+            'judul AS \'' . __('Title') . '\'',
             'LEFT(isi, 200) AS \'' . __('Content Preview') . '\'',
             'status AS \'' . __('Status') . '\'',
             'gambar AS \'' . __('Image') . '\'',
             'created_at AS \'' . __('Created') . '\'',
-            'updated_at AS \'' . __('Last Update') . '\'');
+            'updated_at AS \'' . __('Last Update') . '\''
+        );
     } else {
-        $datagrid->setSQLColumn('judul AS \'' . __('Title') . '\'',
+        $datagrid->setSQLColumn(
+            'judul AS \'' . __('Title') . '\'',
             'LEFT(isi, 200) AS \'' . __('Content Preview') . '\'',
             'status AS \'' . __('Status') . '\'',
             'created_at AS \'' . __('Created') . '\'',
-            'updated_at AS \'' . __('Last Update') . '\'');
+            'updated_at AS \'' . __('Last Update') . '\''
+        );
     }
     $datagrid->invisible_fields = array(0);
     $datagrid->setSQLorder('created_at DESC');
-    
+
     // is there any search
-    if (isset($_GET['keywords']) AND $_GET['keywords']) {
+    if (isset($_GET['keywords']) and $_GET['keywords']) {
         $keywords = $dbs->escape_string(trim($_GET['keywords']));
-        if ($_GET['field'] != '0' AND in_array($_GET['field'], array('judul', 'isi'))) {
+        if ($_GET['field'] != '0' and in_array($_GET['field'], array('judul', 'isi'))) {
             $field = $_GET['field'];
             $str_criteria .= " AND $field LIKE '%$keywords%'";
         } else {
             $str_criteria .= " AND (judul LIKE '%$keywords%' OR isi LIKE '%$keywords%')";
         }
     }
-    
+
     if (isset($_GET['status']) && $_GET['status'] != '') {
         $status = $dbs->escape_string($_GET['status']);
         $str_criteria .= " AND status = '$status'";
     }
 
     $datagrid->setSQLcriteria($str_criteria);
-    
+
     // set table and table header attributes
     $datagrid->table_attr = 'id="dataList" class="s-table table"';
     $datagrid->table_header_attr = 'class="dataListHeader" style="font-weight: bold;"';
     // set delete proccess URL
     $datagrid->chbox_form_URL = $_SERVER['PHP_SELF'];
     $datagrid->debug = true;
-    
+
     // modify image column to show thumbnail
-    if ($can_read AND $can_write) {
+    if ($can_read and $can_write) {
         $datagrid->modifyColumnContent(4, 'callback{showNewsImage}');
     } else {
         $datagrid->modifyColumnContent(3, 'callback{showNewsImage}');
     }
-    
+
     // modify status column to show badge
-    if ($can_read AND $can_write) {
+    if ($can_read and $can_write) {
         $datagrid->modifyColumnContent(3, 'callback{showStatusBadge}');
     } else {
         $datagrid->modifyColumnContent(2, 'callback{showStatusBadge}');
     }
-    
+
     // modify content preview
-    if ($can_read AND $can_write) {
+    if ($can_read and $can_write) {
         $datagrid->modifyColumnContent(2, 'callback{stripTagsAndTrim}');
     } else {
         $datagrid->modifyColumnContent(1, 'callback{stripTagsAndTrim}');
     }
 
     // put the result into variables
-    $datagrid_result = $datagrid->createDataGrid($dbs, $table_spec, $news_result_num, ($can_read AND $can_write));
-    
-    if (isset($_GET['keywords']) AND $_GET['keywords']) {
+    $datagrid_result = $datagrid->createDataGrid($dbs, $table_spec, $news_result_num, ($can_read and $can_write));
+
+    if (isset($_GET['keywords']) and $_GET['keywords']) {
         $msg = str_replace('{result->num_rows}', $datagrid->num_rows, __('Found <strong>{result->num_rows}</strong> from your keywords'));
         echo '<div class="infoBox">' . $msg . ' : "' . htmlentities($_GET['keywords']) . '"<div>' . __('Query took') . ' <b>' . $datagrid->query_time . '</b> ' . __('second(s) to complete') . '</div></div>';
     }
-    
+
     echo $datagrid_result;
 }
 
 // Helper functions for datagrid
-function showNewsImage($value, $row) {
+function showNewsImage($value, $row)
+{
     if (!empty($value) && file_exists(IMGBS . 'docs/' . $value)) {
         return '<img src="' . SWB . 'lib/minigalnano/createthumb.php?filename=images/docs/' . urlencode($value) . '&width=50" class="img-thumbnail" alt="News image">';
     }
     return '<img src="' . SWB . 'lib/minigalnano/createthumb.php?filename=images/default/image.png&width=50" class="img-thumbnail" alt="No image">';
 }
 
-function showStatusBadge($value, $row) {
+function showStatusBadge($value, $row)
+{
     if ($value == 'publish') {
         return '<span class="badge badge-success">' . __('Published') . '</span>';
     }
     return '<span class="badge badge-warning">' . __('Draft') . '</span>';
 }
 
-function stripTagsAndTrim($value, $row) {
+function stripTagsAndTrim($value, $row)
+{
     $value = strip_tags($value);
     if (strlen($value) > 200) {
         $value = substr($value, 0, 200) . '...';
