f5d68c99b7fc992cfe2c2516ee67a84e881b6461
IMAM SODIK <imamsodik00@gmail.com>
2026-06-06 21:27:57 +0700

    adsf

--- admin/modules/informasi/index.php
+++ admin/modules/informasi/index.php
@@ -558,14 +558,10 @@ if (isset($_POST['detail']) OR (isset($_GET['action']) AND $_GET['action'] == 'd
 
 // Helper functions for datagrid
 function showNewsImage($value, $row) {
-    // The callback might be passing the column value and the entire record as array
-    $image_filename = is_array($row) && isset($row['gambar']) ? $row['gambar'] : $value;
-    var_dump($image_filename);
-    
-    if (!empty($image_filename) && !is_object($image_filename) && file_exists(IMGBS . 'docs/' . $image_filename)) {
-        return '<img src="' . SWB . 'lib/minigalnano/createthumb.php?filename=images/docs/' . urlencode($image_filename) . '&width=50" class="img-thumbnail" alt="News image">';
-    }
-    return '<img src="' . SWB . 'lib/minigalnano/createthumb.php?filename=images/default/image.png&width=50" class="img-thumbnail" alt="No image">';
+    echo '<pre>';
+    var_dump(func_get_args());
+    echo '</pre>';
+    die();
 }
 
 function showStatusBadge($value, $row) {
